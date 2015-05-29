5de0f8361d67eed9a1a7eff45a759022073ac255
Ted Stern <dodecatheon@gmail.com>
2015-05-28 17:05:23 -0700

    clarify printout for interactive mode

--- doomsday.py
+++ doomsday.py
@@ -37,17 +37,6 @@ def dayofweek(year, month, day, do_print=False):
     # Year correction within century using the dozens rule
     yc = yy // 12 + yy % 12 + (yy%12)//4
 
-    if do_print:
-        # Alternative calculation for yc
-        oddp11 = (yy + (yy%2)*11) // 2
-        print("({} + {}*11) / 2 = {}".format(yy, yy%2,oddp11))
-        o1 = oddp11
-        oddp11 = (o1 + (o1%2)*11) % 7
-        print("({} + {}*11) % 7 = {}".format(o1, o1%2, oddp11))
-        o2 = oddp11
-        oddp11 = 7 - oddp11
-        print("7 - {} = {}".format(o2, oddp11))
-
     # Doomsday for the year
     pi_day = (c + yc) % 7
 
@@ -62,13 +51,31 @@ def dayofweek(year, month, day, do_print=False):
                    4:"Thursday",
                    5:"Friday",
                    6:"Saturday"}
-        print("Year in century (yy) =", yy)
+        #
+        print("\nYear in century (yy) =", yy)
         print("Century correction (c) =", c)
-        print("yc = yy/12 + yy%12 + (yy%12)/4 =",
+        print("\nYear in century correction (yc), using the dozens rule:")
+        print("\tyc = yy/12 + yy%12 + (yy%12)/4 =",
               yy//12, "+", yy%12, "+", (yy%12)//4, "=", yc)
-        print("Pi day for the year = (c + yc) % 7 =",
+        # Alternative calculation for yc
+        print(("\nCalculating the year-in-century correction with "
+               "the 'odd plus 11' rule:"))
+        oddp11 = (yy + (yy%2)*11) // 2
+        print("\t({} + {}*11) / 2 = {}".format(yy, yy%2,oddp11))
+        o1 = oddp11
+        oddp11 = (o1 + (o1%2)*11) % 7
+        print("\t({} + {}*11) % 7 = {}".format(o1, o1%2, oddp11))
+        o2 = oddp11
+        oddp11 = 7 - oddp11
+        print("\t7 - {} = {}".format(o2, oddp11))
+        print("\nPi day for the year = (c + yc) % 7 =",
               pi_day, "({})".format(dayname[pi_day]))
-        print("\n\t{:04}/{:02}/{:02} occurs on {}\n".format(year,
+
+        print("\nPi day reference for the month is {:02}/{:02}".format(month,dd))
+
+        print("Specified day is {} days from the monthly pi-day".format(day - dd))
+
+        print("\n\t{:04}/{:02}/{:02} falls on a {}\n".format(year,
                                                             month,
                                                             day,
                                                             dayname[weekday]))
