31d7352e3ca8cf91f75ece5fe281d1f406da5021
Ted Stern <dodecatheon@gmail.com>
2015-07-01 14:46:04 -0700

    Show DHMS approximations for each p/q combo

--- meaneqyr.py
+++ meaneqyr.py
@@ -20,6 +20,13 @@ def gregtuple(year):
 def seconds(h, m, s):
     return h*3660. + m*60. + s
 
+def frac2dhms(x):
+    d = int(x)
+    h = int((x - d) * 24.)
+    m = int((x - d - h/24.) * 1440.)
+    s = ((x - d - h/24. - m/1440.) * 86400.)
+    return (d,h,m,s)
+
 if __name__ == "__main__":
     import sys
     import contfrac
@@ -46,6 +53,10 @@ if __name__ == "__main__":
     print("\tMean eq. year ~= ", days, "/", years)
     print("\twith", leaps, "leap years.\n")
     print("\t{}".format(days), "days modulo 7 =", days % 7)
+    dd, hh, mm, ss = frac2dhms(days / float(years))
+    print("\tMean eq. year without adjustment = {} days, {} hours, {} minutes, {} seconds\n".format(dd,hh,mm,ss))
+
+
 
     approx = (days + adjust) / float(years)
     print("\tAdjusted days =", days + adjust)
@@ -54,12 +65,22 @@ if __name__ == "__main__":
     print("\tDecimal mean eq. year =", approx)
 
     frac = (days + adjust) / float(years)
+    dd, hh, mm, ss = frac2dhms(frac)
 
-    dd = int(frac)
-    hh = int((frac - dd) * 24.)
-    mm = int((frac - dd - hh/24.) * 1440.)
-    ss = ((frac - dd - hh/24. - mm/1440.) * 86400.)
-
-    print("Mean eq. year = {} days, {} hours, {} minutes, {} seconds".format(dd,hh,mm,ss))
+    print(("Mean eq. year = {} days, "
+           "{} hours, "
+           "{} minutes, "
+           "{} seconds").format(dd,hh,mm,ss))
 
     contfrac.main(frac)
+
+    fmt = ("\tx ~= p / q = {p:10} / {q:10} = {d:03} "
+           "{h:02}:{m:02}:{s:07.4f} "
+           ", p days modulo 7 = {r:1}")
+
+    print("\nContinued fraction approximations in DHMS form:")
+
+    for a, p, q in contfrac.continued_fraction(frac):
+        d, h, m, s = frac2dhms(p / float(q))
+        r = p % 7
+        print(fmt.format(p=p, q=q, d=d, h=h, m=m, s=s, r=r))
